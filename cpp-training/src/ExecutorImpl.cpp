2fd6f8cc853518c0a99eedadd064ed70d1d6e3b9
www <1684723594@qq.com>
2025-11-02 15:40:49 +0800

    application of table-driven

--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -1,5 +1,6 @@
 #include "ExecutorImpl.hpp"
 #include <memory>
+#include <unordered_map>
 #include "Command.hpp"
 
 namespace adas
@@ -14,25 +15,19 @@ namespace adas
 
     void ExecutorImpl::Execute(const std::string& commands) noexcept
     {
+        std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
+        cmderMap.emplace('M', std::make_unique<MoveCommand>());
+        cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
+        cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
+        cmderMap.emplace('F', std::make_unique<FastCommand>());
         for (const auto cmd : commands) {
-
-            std::unique_ptr<ICommand> cmder;
-
-            if (cmd == 'M') {
-                cmder = std::make_unique<MoveCommand>();
-            } else if (cmd == 'L') {
-                cmder = std::make_unique<TurnLeftCommand>();
-            } else if (cmd == 'R') {
-                cmder = std::make_unique<TurnRightCommand>();
-            } else if(cmd=='F'){
-                cmder=std::make_unique<FastCommand>();
-            }
-
-            if(cmder){
-                cmder->DoOperate(poseHandler);
-            }
-            }
+        const auto it = cmderMap.find(cmd);
+        if (it != cmderMap.end())
+        {
+            it->second->DoOperate(poseHandler);
         }
+        }
+    }
 
     Pose ExecutorImpl::Query() const noexcept
     {
