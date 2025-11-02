4bd053543049d4dc50a95fcc5c21e165a884dd7b
www <1684723594@qq.com>
2025-11-02 16:02:31 +0800

    implementation of function objects

--- cpp-training/src/Command.hpp
+++ cpp-training/src/Command.hpp
@@ -3,83 +3,52 @@
 #include "PoseHandler.hpp"
 #include <functional>
 
-namespace adas{
-//class ICommand
-//{
-//public:
-//    virtual ~ICommand() = default;
-//    virtual void DoOperate(PoseHandler& poseHandler) const noexcept = 0;
-//};
+namespace adas
+{
+
 class MoveCommand final
 {
 public:
-    //void DoOperate(PoseHandler& poseHandler) const noexcept
-    //{
-    //    if (poseHandler.IsFast()) {
-    //        poseHandler.Move();
-    //    }
-    //    poseHandler.Move();
-    //}
-
-    const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
-        if (poseHandler.IsFast())
-        {
+    void operator()(PoseHandler& poseHandler) const noexcept
+    {
+        if (poseHandler.IsFast()) {
             poseHandler.Move();
         }
         poseHandler.Move();
-    };
+    }
 };
 
 class TurnLeftCommand final
 {
 public:
-    //void DoOperate(PoseHandler& poseHandler) const noexcept
-    //{
-    //    if (poseHandler.IsFast()) {
-    //        poseHandler.Move();
-    //    }
-    //    poseHandler.TurnLeft();
-    //}
-
-    const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
-        if (poseHandler.IsFast())
-        {
+    void operator()(PoseHandler& poseHandler) const noexcept
+    {
+        if (poseHandler.IsFast()) {
             poseHandler.Move();
         }
         poseHandler.TurnLeft();
-    };
+    }
 };
 
 class TurnRightCommand final
 {
 public:
-    //void DoOperate(PoseHandler& poseHandler) const noexcept
-    //{
-    //    if (poseHandler.IsFast()) {
-    //        poseHandler.Move();
-    //    }
-    //    poseHandler.TurnRight();
-    //}
-
-    const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
-        if (poseHandler.IsFast())
-        {
+    void operator()(PoseHandler& poseHandler) const noexcept
+    {
+        if (poseHandler.IsFast()) {
             poseHandler.Move();
         }
         poseHandler.TurnRight();
-    };
+    }
 };
 
 class FastCommand final
 {
 public:
-    //void DoOperate(PoseHandler& poseHandler) const noexcept override
-    //{
-    //    poseHandler.Fast();
-    //}
-
-    const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
+    void operator()(PoseHandler& poseHandler) const noexcept
+    {
         poseHandler.Fast();
-    };
+    }
 };
-};
+
+};  // namespace adas
--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -16,15 +16,10 @@ namespace adas
     void ExecutorImpl::Execute(const std::string& commands) noexcept
     {
         std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap;
-        MoveCommand moveCommand;
-        cmderMap.emplace('M', moveCommand.operate);
-        TurnLeftCommand turnLeftCommand;
-        cmderMap.emplace('L', turnLeftCommand.operate);
-        TurnRightCommand turnRightCommand;
-        cmderMap.emplace('R', turnRightCommand.operate);
-
-        FastCommand fastCommand;
-        cmderMap.emplace('F', fastCommand.operate);
+        cmderMap.emplace('M', MoveCommand());
+        cmderMap.emplace('L', TurnLeftCommand());
+        cmderMap.emplace('R', TurnRightCommand());
+        cmderMap.emplace('F', FastCommand());
 
         for (const auto cmd : commands) {
             const auto it = cmderMap.find(cmd);
@@ -40,4 +35,4 @@ namespace adas
         return poseHandler.Query();
     }
 
-}  // namespace adas
+}
