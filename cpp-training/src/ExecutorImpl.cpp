1912c0c061df2ef6656be836a46a89383fa864a2
www <1684723594@qq.com>
2025-11-02 15:51:57 +0800

    functional programming

--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -15,17 +15,23 @@ namespace adas
 
     void ExecutorImpl::Execute(const std::string& commands) noexcept
     {
-        std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
-        cmderMap.emplace('M', std::make_unique<MoveCommand>());
-        cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
-        cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
-        cmderMap.emplace('F', std::make_unique<FastCommand>());
+        std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap;
+        MoveCommand moveCommand;
+        cmderMap.emplace('M', moveCommand.operate);
+        TurnLeftCommand turnLeftCommand;
+        cmderMap.emplace('L', turnLeftCommand.operate);
+        TurnRightCommand turnRightCommand;
+        cmderMap.emplace('R', turnRightCommand.operate);
+
+        FastCommand fastCommand;
+        cmderMap.emplace('F', fastCommand.operate);
+
         for (const auto cmd : commands) {
-        const auto it = cmderMap.find(cmd);
-        if (it != cmderMap.end())
-        {
-            it->second->DoOperate(poseHandler);
-        }
+            const auto it = cmderMap.find(cmd);
+            if (it != cmderMap.end())
+            {
+                it->second(poseHandler);
+            }
         }
     }
 
